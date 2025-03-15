d764688d6cea554712630ef1351b4db268f04f5b
Thalia Nero <chrisvn00@gmail.com>
2025-03-14 22:15:12 -0500

    Add Functor for StateT

--- src/control/state.rs
+++ src/control/state.rs
@@ -1,26 +1,34 @@
 use std::marker::PhantomData;
 
-use crate::{expression::{ExprCapable, Expression, FnType}, data::Type};
+use crate::{
+    data::{
+        pair::{map_snd, Pair},
+        Type,
+    },
+    expression::{ExprCapable, Expression, FnType},
+    function::compose,
+    ExprType,
+};
 
-use super::{TypeCtor, identity::Identity, Functor};
+use super::{identity::Identity, Functor, TypeCtor};
 
-pub type StateT<S: ExprCapable, T: TypeCtor, A: ExprCapable> = FnType<S, T::Apply<(Expression<S>, Expression<A>)>>;
-
-pub type State<S: ExprCapable, A: ExprCapable> = StateT<S, Identity, A>;
+pub type StateFn<S, T, A> = ExprType!(S => type <T as TypeCtor>::Apply<Pair<S, A>>);
 
 #[derive(Debug, Clone)]
-pub struct Canonical<S: Type, T: TypeCtor>(PhantomData<(S, T)>);
+pub struct StateT<S: Type, T: TypeCtor>(PhantomData<(S, T)>);
+pub type State<S> = StateT<S, Identity>;
 
-impl<S: Type, T: TypeCtor> ExprCapable for Canonical<S, T> {}
+impl<S: Type, T: TypeCtor> ExprCapable for StateT<S, T> {}
 
-impl<S: Type, T: TypeCtor> TypeCtor for Canonical<S, T> {
-    type Apply<A: ExprCapable> = StateT<S::Apply, T, A>;
+impl<S: Type, T: TypeCtor> TypeCtor for StateT<S, T> {
+    type Apply<A: ExprCapable> = StateFn<S::Apply, T, A>;
 }
 
-impl<S: Type, T: Functor> Functor for Canonical<S, T> {
-    // map f st s = map (\(s', a) -> (s', f a)) (st s)
+impl<S: Type, T: Functor> Functor for StateT<S, T> {
+    // map f st s = map (map_snd f) (st s)
+    // map f = (.) (map (map_snd f))
     fn map<A: ExprCapable, B: ExprCapable>(
     ) -> Expression<FnType<FnType<A, B>, FnType<Self::Apply<A>, Self::Apply<B>>>> {
-        todo!()
+        compose().compose(T::map()).compose(map_snd())
     }
 }
